cb80001a823271687ef479326db4c05e2263a7ad
SandraM2023 <126633471+SandraM2023@users.noreply.github.com>
2023-02-28 22:47:51 -0300

    Add files via upload

---
+++ Proyecto/Eventos/eventos.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById('boton').addEventListener('click', function() {
+    console.log("Mostrar link de Facebook");
+    document.getElementById("demo").innerHTML = "Facebook";
+    document.getElementById('demo').style.display = 'block';
+});
+
